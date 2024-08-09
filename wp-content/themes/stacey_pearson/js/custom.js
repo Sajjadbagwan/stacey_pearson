bb7bd73c510d2061edc9943c1f3628ec3bda78dd
Aamir-KGN <aamir.patel@kgn-software.com>
2024-08-09 17:59:36 +0530

    Update custom.js

--- wp-content/themes/stacey_pearson/js/custom.js
+++ wp-content/themes/stacey_pearson/js/custom.js
@@ -101,7 +101,7 @@ jQuery( document ).ready(function() {
           jQuery("html").addClass("popupopen");
           setTimeout(function(){
             jQuery(".mainpopup .popupslider").slick('setPosition');
-            jQuery(".mainpopup .sync-slider").slick('setPosition');
+            // jQuery(".mainpopup .sync-slider").slick('setPosition');
             jQuery(".mainpopup .popupslider .imagebox .contentbox").css("display","block");
           },1000)          
         }); 
